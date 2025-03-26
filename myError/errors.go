542d0375ea717d2c7fbfe1b2145975b57b861763
ericsanto <ericjesus403@gmail.com>
2025-03-26 09:25:24 -0300

    feat: create struct Myerror for send personalized error

---
+++ myError/errors.go
@@ -0,0 +1,22 @@
+package myerror
+
+import "fmt"
+
+
+type ErrorApp struct {
+
+  Message   string  `json:"message"`
+  Code      uint     `json:"code"`
+  Timestamp string  `json:"timestamp"`
+
+}
+
+func NewError(message, timestamp string, code uint) *ErrorApp {
+  return &ErrorApp{Message: message, Code: code, Timestamp: timestamp}
+}
+
+func(e *ErrorApp) Error() string {
+  return fmt.Sprintf("%d, %s, %s", e.Code, e.Message, e.Timestamp)
+}
+
+  
