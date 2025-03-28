9452762beb1b2e03b556a5f5ca0e249ab6379cd8
ericsanto <ericjesus403@gmail.com>
2025-03-28 11:16:01 -0300

    feat: add autoincrement in model on field Id

--- internal/models/entities/soil_type.go
+++ internal/models/entities/soil_type.go
@@ -2,8 +2,8 @@ package entities
 
 
 type SoilTypeEntity struct {
-  Id          uint  `gorm:"primaryKey; autoIncrement"` 
-  Name        string `gorm:"size:100;not null"`
+  Id          uint  `gorm:"primaryKey;autoIncrement"` 
+  Name        string `gorm:"size:100;not null;unique"`
   Description string `gorm:"type:text;not null"`
   AgricultureCultures []AgricultureCultureEntity `gorm:"foreignKey:SoilTypeId"`
 }
