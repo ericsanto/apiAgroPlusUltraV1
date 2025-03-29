5afaf96d564376321d87bc145afad8145fc22996
ericsanto <ericjesus403@gmail.com>
2025-03-29 13:42:25 -0300

    feat: create entity PestEntity

---
+++ internal/models/entities/pest_entity.go
@@ -0,0 +1,10 @@
+package entities
+
+
+type PestEntity struct {
+
+  Id             uint           `gorm:"primaryKey;autoIncrement"`
+  Name           string         `gorm:"size:100;not null"`
+  TypePestId     uint           
+  TypePestEntity TypePestEntity `gorm:"foreignKey:TypePestId;references:Id"` 
+}
