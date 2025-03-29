5b8cefac347da50e408c2c2602100d8e4cfd915f
ericsanto <ericjesus403@gmail.com>
2025-03-29 13:55:26 -0300

    feat: implements AgricultureCultureInterface interface

--- internal/repositories/agriculture_culture.go
+++ internal/repositories/agriculture_culture.go
@@ -7,6 +7,14 @@ import (
 	"gorm.io/gorm"
 )
 
+type AgricultureCultureInterface interface {
+  
+  FindAllAgricultureCulture() ([]entities.AgricultureCultureEntity, error)
+  FindByIdAgricultureCulture(id uint) (entities.AgricultureCultureEntity, error)
+  CreateAgricultureCulture(agricultureCulture *entities.AgricultureCultureEntity)
+  UpdateAgricultureCulture(id uint, agricultureCulture entities.AgricultureCultureEntity) error
+  DeleteAgricultureCulture(id uint)
+}
 
 type AgricultureCultureRepository struct {
 
@@ -18,7 +26,7 @@ func NewAgricultureCultureRepository(db *gorm.DB) *AgricultureCultureRepository 
   return &AgricultureCultureRepository{db:db}
 }
 
-func(a *AgricultureCultureRepository) FindAll() ([]entities.AgricultureCultureEntity, error) {
+func(a *AgricultureCultureRepository) FindAllAgricultureCulture() ([]entities.AgricultureCultureEntity, error) {
 
   var agricultureCultures []entities.AgricultureCultureEntity
   err := a.db.Find(&agricultureCultures)
@@ -31,7 +39,7 @@ func(a *AgricultureCultureRepository) FindAll() ([]entities.AgricultureCultureEn
 
 }
 
-func(a *AgricultureCultureRepository) FindById(id uint) (entities.AgricultureCultureEntity, error) {
+func(a *AgricultureCultureRepository) FindByIdAgricultureCulture(id uint) (entities.AgricultureCultureEntity, error) {
 
   var agricultureCulture entities.AgricultureCultureEntity
 
@@ -44,7 +52,7 @@ func(a *AgricultureCultureRepository) FindById(id uint) (entities.AgricultureCul
   return agricultureCulture, nil
 }
 
-func(a *AgricultureCultureRepository) Create(agriculutreCulture *entities.AgricultureCultureEntity) error {
+func(a *AgricultureCultureRepository) CreateAgricultureCulture(agriculutreCulture *entities.AgricultureCultureEntity) error {
 
 
   err := a.db.Create(&agriculutreCulture)
@@ -57,9 +65,9 @@ func(a *AgricultureCultureRepository) Create(agriculutreCulture *entities.Agricu
 }
 
 
-func(r *AgricultureCultureRepository) Update(id uint, agricultureCultureEntity entities.AgricultureCultureEntity) error {
+func(r *AgricultureCultureRepository) UpdateAgricultureCulture(id uint, agricultureCultureEntity entities.AgricultureCultureEntity) error {
 
-  agricultureCultureExists, err := r.FindById(id)
+  agricultureCultureExists, err := r.FindByIdAgricultureCulture(id)
   if err != nil {
     return fmt.Errorf(err.Error())
   }
@@ -72,9 +80,9 @@ func(r *AgricultureCultureRepository) Update(id uint, agricultureCultureEntity e
   return nil
 }
 
-func(r *AgricultureCultureRepository) Delete(id uint) error {
+func(r *AgricultureCultureRepository) DeleteAgricultureCulture(id uint) error {
 
-  agricultureCultureExists, err := r.FindById(id)
+  agricultureCultureExists, err := r.FindByIdAgricultureCulture(id)
   if err != nil {
     return fmt.Errorf(err.Error())
   }
