4448dcc423b67d509fe87cd485ce6da58e298372
ericsanto <ericjesus403@gmail.com>
2025-04-02 09:16:48 -0300

    fix: adjusts the relationship between the Agricultural Culture table

--- internal/models/entities/pest_entity.go
+++ internal/models/entities/pest_entity.go
@@ -1,10 +1,9 @@
 package entities
 
-
 type PestEntity struct {
-
-  Id             uint           `gorm:"primaryKey;autoIncrement"`
-  Name           string         `gorm:"size:100;not null"`
-  TypePestId     uint           
-  TypePestEntity TypePestEntity `gorm:"foreignKey:TypePestId;references:Id"` 
+	Id                  uint   `gorm:"primaryKey;autoIncrement"`
+	Name                string `gorm:"size:100;not null"`
+	TypePestId          uint
+	TypePestEntity      TypePestEntity           `gorm:"foreignKey:TypePestId;references:Id"`
+	AgricultureCultures []PestAgricultureCulture `gorm:"foreignKey:PestId"`
 }
