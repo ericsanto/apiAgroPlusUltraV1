87d5a31504ad79d6c221d04268386ed8dce4ef83
ericsanto <ericjesus403@gmail.com>
2025-04-08 12:20:50 -0300

    feat: create function that handles http errors

---
+++ myError/http_errors.go
@@ -0,0 +1,24 @@
+package myerror
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// if err := c.ShouldBindJSON(&requestAgricultureCultureIrrigation); err != nil {
+// 	c.JSON(http.StatusBadRequest, myerror.ErrorApp{
+// 		Code:      http.StatusBadRequest,
+// 		Message:   "body da requisição é inválido",
+// 		Timestamp: time.Now().Format(time.RFC3339),
+// 	})
+// 	return
+// }
+
+func HttpErrors(statusCode int, message interface{}, c *gin.Context) {
+	c.AbortWithStatusJSON(statusCode, ErrorApp{
+		Code:      uint(statusCode),
+		Message:   message,
+		Timestamp: time.Now().Format(time.RFC3339),
+	})
+}
