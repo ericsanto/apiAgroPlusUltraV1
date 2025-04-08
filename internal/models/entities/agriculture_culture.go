e8da5c04572edf7c568ee2baf8f57cff0fb1ca76
ericsanto <ericjesus403@gmail.com>
2025-04-08 12:29:50 -0300

    refactor

--- internal/models/entities/agriculture_culture.go
+++ internal/models/entities/agriculture_culture.go
@@ -5,13 +5,14 @@ type AgricultureCultureEntity struct {
 	Name                      string `gorm:"size:255;not null"`
 	NameCientific             string `gorm:"size:255;not null"`
 	SoilTypeId                uint
-	SoilTypeEntity            SoilTypeEntity           `gorm:"foreignKey:SoilTypeId;references:Id"`
-	PhIdealSoil               float32                  `gorm:"not null"`
-	MaxTemperature            float32                  `gorm:"not null"`
-	MinTemperature            float32                  `gorm:"not null"`
-	ExcellentTemperature      float32                  `gorm:"not null"`
-	WeeklyWaterRequirementMax float32                  `gorm:"not null"`
-	WeeklyWaterRequirementMin float32                  `gorm:"not null"`
-	SunlightRequirement       uint                     `gorm:"not null"`
-	Pests                     []PestAgricultureCulture `gorm:"foreignKey:AgricultureCultureId"`
+	SoilTypeEntity            SoilTypeEntity                 `gorm:"foreignKey:SoilTypeId;references:Id"`
+	PhIdealSoil               float32                        `gorm:"not null"`
+	MaxTemperature            float32                        `gorm:"not null"`
+	MinTemperature            float32                        `gorm:"not null"`
+	ExcellentTemperature      float32                        `gorm:"not null"`
+	WeeklyWaterRequirementMax float32                        `gorm:"not null"`
+	WeeklyWaterRequirementMin float32                        `gorm:"not null"`
+	SunlightRequirement       uint                           `gorm:"not null"`
+	Pests                     []PestAgricultureCulture       `gorm:"foreignKey:AgricultureCultureId"`
+	IrrigationRecomended      []AgricultureCultureIrrigation `gorm:"foreignKey:AgricultureCultureId"`
 }
