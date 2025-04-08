f22a391fc0aa283dfe76844d3ad39b730d256cf6
ericsanto <ericjesus403@gmail.com>
2025-04-08 12:26:16 -0300

    feat: create routerGroup

---
+++ internal/routes/routesGroup/sustainable_pest_control.go
@@ -0,0 +1,23 @@
+package routesgroup
+
+import (
+	"github.com/ericsanto/apiAgroPlusUltraV1/config"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/controllers"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/middlewares"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/repositories"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouterSustainablePestControl(r *gin.Engine) {
+
+	repositorySustainablePestControl := repositories.NewSustainablePestControlRepository(config.DB)
+	serviceSustainablePestControl := services.NewSustainablePestControlService(repositorySustainablePestControl)
+	controllerSustainablePestControl := controllers.NewSustainablePestControlController(serviceSustainablePestControl)
+
+	routerSustainablePestControl := r.Group("/v1/controle-de-pragas")
+	routerSustainablePestControl.GET("/", controllerSustainablePestControl.GetAllSustainablePestControl)
+	routerSustainablePestControl.POST("/", controllerSustainablePestControl.PostSustainablePestControl)
+	routerSustainablePestControl.GET("/:id", middlewares.ValidateIdParam("id"), controllerSustainablePestControl.GetFindByIdSustainablePestControl)
+	routerSustainablePestControl.PUT(":id/", middlewares.ValidateIdParam("id"), controllerSustainablePestControl.PutSustainablePestControl)
+}
