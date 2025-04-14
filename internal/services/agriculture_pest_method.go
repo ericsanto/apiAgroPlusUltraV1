04166476586d3a82e0d327e58ad5f9a2f44fdf21
Éric Santos <ericjesus403@gmail.com>
2025-04-14 10:51:19 -0300

    feat: create service AgricultureCulturePestMethodService to work with the AgricultureCulturePestMethodRepository

---
+++ internal/services/agriculture_pest_method.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/models/entities"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/models/requests"
+
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/models/responses"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/repositories"
+)
+
+type AgricultureCulturePestMethodService struct {
+	agricultureCulturePestMethdRepository *repositories.AgricultureCulturePestMethodRepository
+}
+
+func NewAgricultureCulturePestMethodService(agricultureCulturePestMethdRepository *repositories.AgricultureCulturePestMethodRepository) *AgricultureCulturePestMethodService {
+	return &AgricultureCulturePestMethodService{agricultureCulturePestMethdRepository: agricultureCulturePestMethdRepository}
+}
+
+func (a *AgricultureCulturePestMethodService) PostAgricultureCulturePestMethod(requestAgricultureCulturePestMethod requests.AgricultureCulturePestMethodRequest) error {
+
+	entityAgricultureCulturePestMethod := entities.AgricultureCulturePestMethodEntity{
+		AgricultureCultureId:     requestAgricultureCulturePestMethod.AgricultureCultureId,
+		PestId:                   requestAgricultureCulturePestMethod.PestId,
+		SustainablePestControlId: requestAgricultureCulturePestMethod.SustainablePestControlId,
+		Description:              requestAgricultureCulturePestMethod.Description,
+	}
+
+	if err := a.agricultureCulturePestMethdRepository.CreateAgricultureCulturePestMethod(entityAgricultureCulturePestMethod); err != nil {
+		return fmt.Errorf("erro: %v", err)
+	}
+
+	return nil
+}
+
+func (a *AgricultureCulturePestMethodService) GetAllAgricultureCulturePestMethod() ([]responses.AgricultureCulturePestMethodResponse, error) {
+
+	result, err := a.agricultureCulturePestMethdRepository.FindAllAgricultureCulturePestMethod()
+	if err != nil {
+		return result, fmt.Errorf("erro: %v", err)
+	}
+
+	return result, nil
+
+}
+
+func (a *AgricultureCulturePestMethodService) GetAllAgricultureCulturePestMethodByParam(cultureName, pestName, methodName interface{}) ([]responses.AgricultureCulturePestMethodResponse, error) {
+
+	responseAgricultureCulturePestMethod, err := a.agricultureCulturePestMethdRepository.FindByQueryParamAgricultureCulturePestMethod(cultureName, pestName, methodName)
+	if err != nil {
+		return responseAgricultureCulturePestMethod, fmt.Errorf("erro: %v", err)
+	}
+
+	return responseAgricultureCulturePestMethod, nil
+}
