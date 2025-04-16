eddb14a1225a8eb35faad9ad25957c6a90bca447
Éric Santos <ericjesus403@gmail.com>
2025-04-16 09:18:19 -0300

    feat: add new attributes in the request and response dto of the AgricultureCulture entity

--- internal/models/requests/agriculture_culture_request.go
+++ internal/models/requests/agriculture_culture_request.go
@@ -1,19 +1,22 @@
 package requests
 
-import "github.com/ericsanto/apiAgroPlusUltraV1/internal/models/entities"
-
+import (
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/enums"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/models/entities"
+)
 
 type AgricultureCultureRequest struct {
-    Name                        string  `json:"name" validate:"required,min=3,max=100"`
-    NameCientific               string  `json:"name_cientific" validate:"required"`
-    SoilTypeId                  uint    `json:"soil_type_id" validate:"required"`
-    SoilType                    entities.SoilTypeEntity
-    PhIdealSoil                 float32 `json:"ph_ideal_soil" validate:"required,min=0,max=14"`
-    MaxTemperature              float32 `json:"max_temperature" validate:"required"`
-    MinTemperature              float32 `json:"min_temperature" validate:"required"`
-    ExcellentTemperature        float32 `json:"excellent_temperature" validate:"required"`
-    WeeklyWaterRequirementMax   float32 `json:"weekly_water_requirement_max" validate:"required,gtfield=WeeklyWaterRequirementMin"`
-    WeeklyWaterRequirementMin   float32 `json:"weekly_water_requirement_min" validate:"required"`
-    SunlightRequirement         uint `json:"sunlight_requirement" validate:"required"`
-
+	Name                      string `json:"name" validate:"required,min=3,max=100"`
+	Variety                   string `json:"variety" validate:"required"`
+	SoilTypeId                uint   `json:"soil_type_id" validate:"required"`
+	SoilType                  entities.SoilTypeEntity
+	Region                    enums.Region  `json:"region" validate:"required"`
+	UseType                   enums.UseType `json:"use_type" validate:"required"`
+	PhIdealSoil               float32       `json:"ph_ideal_soil" validate:"required,min=0,max=14"`
+	MaxTemperature            float32       `json:"max_temperature" validate:"required"`
+	MinTemperature            float32       `json:"min_temperature" validate:"required"`
+	ExcellentTemperature      float32       `json:"excellent_temperature" validate:"required"`
+	WeeklyWaterRequirementMax float32       `json:"weekly_water_requirement_max" validate:"required,gtfield=WeeklyWaterRequirementMin"`
+	WeeklyWaterRequirementMin float32       `json:"weekly_water_requirement_min" validate:"required"`
+	SunlightRequirement       uint          `json:"sunlight_requirement" validate:"required"`
 }
--- internal/models/responses/agriculture_culture_response.go
+++ internal/models/responses/agriculture_culture_response.go
@@ -1,19 +1,19 @@
 package responses
 
-
+import "github.com/ericsanto/apiAgroPlusUltraV1/internal/enums"
 
 type AgricultureCultureResponse struct {
-
-  Id                         uint            
-  Name                       string         
-  NameCientific              string         
-  SoilTypeId                 uint
-  PhIdealSoil                float32        
-  MaxTemperature             float32        
-  MinTemperature             float32        
-  ExcellentTemperature       float32        
-  WeeklyWaterRequirememntMax float32        
-  WeeklyWaterRequirememntMin float32        
-  SunlightRequirement        uint           
+	Id                         uint
+	Name                       string
+	Variety                    string
+	Region                     enums.Region
+	UseType                    enums.UseType
+	SoilTypeId                 uint
+	PhIdealSoil                float32
+	MaxTemperature             float32
+	MinTemperature             float32
+	ExcellentTemperature       float32
+	WeeklyWaterRequirememntMax float32
+	WeeklyWaterRequirememntMin float32
+	SunlightRequirement        uint
 }
-
