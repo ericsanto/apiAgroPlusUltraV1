f4d6dee08543df57188bcf390d81ada1d4eaece5
Éric Santos <ericjesus403@gmail.com>
2025-04-18 14:38:46 -0300

    feat: adds Planting field and relates it to PlantingEntity table

--- internal/models/entities/batch.go
+++ internal/models/entities/batch.go
@@ -1,8 +1,9 @@
 package entities
 
 type BatchEntity struct {
-	ID   uint    `gorm:"primaryKey;autoIncrement"`
-	Name string  `gorm:"size:50;not null;unique"`
-	Area float32 `gorm:"not null"`
-	Unit string  `gorm:"size:10;not null"`
+	ID       uint             `gorm:"primaryKey;autoIncrement"`
+	Name     string           `gorm:"size:50;not null;unique"`
+	Area     float32          `gorm:"not null"`
+	Unit     string           `gorm:"size:10;not null"`
+	Planting []PlantingEntity `gorm:"foreignKey:BatchID"`
 }
