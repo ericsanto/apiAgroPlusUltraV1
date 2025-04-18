564d97ff928d0d7c394deec74caff54714a97cf0
Éric Santos <ericjesus403@gmail.com>
2025-04-18 19:04:03 -0300

    feat: adds ProductionCost field and relates it to ProductinoCostEntity table

--- internal/models/entities/planting.go
+++ internal/models/entities/planting.go
@@ -6,6 +6,7 @@ type PlantingEntity struct {
 	ID                   uint `gorm:"primaryKey;autoIncrement"`
 	BatchID              uint
 	AgricultureCultureID uint
-	IsPlanting           bool      `gorm:"default:false"`
-	StartDatePlanting    time.Time `gorm:"type:timestamp"`
+	IsPlanting           bool                   `gorm:"default:false"`
+	StartDatePlanting    time.Time              `gorm:"type:timestamp"`
+	ProductionCost       []ProductionCostEntity `gorm:"foreignKey:PlantingID"`
 }
