0cad2aa484aade2f08be900ebd0f797ad5248d91
Éric Santos <ericjesus403@gmail.com>
2025-04-19 17:46:26 -0300

    feat: create controller SalePlantingController to work with http requests

---
+++ internal/controllers/sale_planting.go
@@ -0,0 +1,148 @@
+package controllers
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/models/requests"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/services"
+	myerror "github.com/ericsanto/apiAgroPlusUltraV1/myError"
+	"github.com/ericsanto/apiAgroPlusUltraV1/validators"
+	"github.com/gin-gonic/gin"
+)
+
+type SalePlantingController struct {
+	salePlantingService *services.SalePlantingService
+}
+
+func NewSalePlantingController(salePlantingService *services.SalePlantingService) *SalePlantingController {
+	return &SalePlantingController{salePlantingService: salePlantingService}
+}
+
+func (s *SalePlantingController) PostSalePlanting(c *gin.Context) {
+
+	var requestSalePlanting requests.SalePlantingRequest
+
+	if err := c.ShouldBindJSON(&requestSalePlanting); err != nil {
+		myerror.HttpErrors(http.StatusBadRequest, "body da requisição é inválido", c)
+		return
+	}
+
+	validate, err := validators.ValidateFieldErrors422UnprocessableEntity(requestSalePlanting)
+	if err != nil {
+		myerror.HttpErrors(http.StatusInternalServerError, "erro no servidor", c)
+		return
+	}
+
+	if len(validate) > 0 {
+		myerror.HttpErrors(http.StatusUnprocessableEntity, validate, c)
+		return
+	}
+
+	if err := s.salePlantingService.PostSalePlanting(requestSalePlanting); err != nil {
+		if errors.Is(err, myerror.ErrDuplicateSale) {
+			myerror.HttpErrors(http.StatusConflict, err.Error(), c)
+			return
+		}
+
+		myerror.HttpErrors(http.StatusInternalServerError, "erro no servidor", c)
+		return
+	}
+
+	c.Status(http.StatusOK)
+
+}
+
+func (s *SalePlantingController) GetAllSalePlanting(c *gin.Context) {
+
+	responsesSalePlanting, err := s.salePlantingService.GetAllSalePlanting()
+	if err != nil {
+		myerror.HttpErrors(http.StatusInternalServerError, "erro no servidor", c)
+		return
+	}
+
+	c.JSON(http.StatusOK, responsesSalePlanting)
+}
+
+func (s *SalePlantingController) GetSalePlantingByID(c *gin.Context) {
+
+	id := validators.GetAndValidateIdMidlware(c, "validatedID")
+
+	responseSalePlanting, err := s.salePlantingService.GetSalePlantingByID(id)
+	if err != nil {
+		if errors.Is(err, myerror.ErrNotFoundSale) {
+			myerror.HttpErrors(http.StatusNotFound, err.Error(), c)
+			return
+		}
+
+		myerror.HttpErrors(http.StatusInternalServerError, "erro no servidor", c)
+		return
+	}
+
+	c.JSON(http.StatusOK, responseSalePlanting)
+}
+
+func (s *SalePlantingController) PutSalePlanting(c *gin.Context) {
+
+	var requestSalePlanting requests.SalePlantingRequest
+
+	if err := c.ShouldBindJSON(&requestSalePlanting); err != nil {
+		myerror.HttpErrors(http.StatusBadRequest, "body da requisição é inválido", c)
+		return
+	}
+
+	validate, err := validators.ValidateFieldErrors422UnprocessableEntity(requestSalePlanting)
+	if err != nil {
+		myerror.HttpErrors(http.StatusInternalServerError, "erro no servidor", c)
+		return
+	}
+
+	if len(validate) > 0 {
+		myerror.HttpErrors(http.StatusUnprocessableEntity, validate, c)
+		return
+	}
+
+	id := validators.GetAndValidateIdMidlware(c, "validatedID")
+
+	if err := s.salePlantingService.PutSalePlanting(id, requestSalePlanting); err != nil {
+
+		switch {
+		case errors.Is(err, myerror.ErrDuplicateSale):
+			myerror.HttpErrors(http.StatusConflict, err.Error(), c)
+			return
+
+		case errors.Is(err, myerror.ErrNotFoundSale):
+			myerror.HttpErrors(http.StatusNotFound, err.Error(), c)
+			return
+
+		case errors.Is(err, myerror.ErrViolatedForeingKey):
+			myerror.HttpErrors(http.StatusConflict, err.Error(), c)
+			return
+		default:
+			myerror.HttpErrors(http.StatusInternalServerError, "erro no servidor", c)
+			return
+		}
+
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func (s *SalePlantingController) DeleteSalePlanting(c *gin.Context) {
+
+	id := validators.GetAndValidateIdMidlware(c, "validatedID")
+
+	if err := s.salePlantingService.DeleteSalePlanting(id); err != nil {
+		switch {
+		case errors.Is(err, myerror.ErrNotFoundSale):
+			myerror.HttpErrors(http.StatusNotFound, err.Error(), c)
+			return
+
+		default:
+			myerror.HttpErrors(http.StatusInternalServerError, "erro no servidor", c)
+			return
+		}
+	}
+
+	c.Status(http.StatusNoContent)
+}
