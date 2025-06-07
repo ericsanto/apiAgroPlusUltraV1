7945fd6a3a0a8efb3895ac524e55346d4d227212
Eric Santos <ericjesus403@gmail.com>
2025-06-07 18:07:09 -0300

    refactor: update UploadFile to accept key parameter and adjust controller accordingly

--- internal/controllers/disease_detect.go
+++ internal/controllers/disease_detect.go
@@ -1,57 +1,111 @@
 package controllers
 
 import (
-	"bytes"
-	"io"
-	"mime/multipart"
+	"context"
+	"errors"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/IBM/sarama"
+	myerror "github.com/ericsanto/apiAgroPlusUltraV1/myError"
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/bucket"
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/jsonutil"
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/kafka"
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/upload"
 	"github.com/gin-gonic/gin"
 )
 
+type ResponseApiPythonDisease struct {
+	Disease string `json:"disease"`
+}
+
+type ResponseApiPythonError struct {
+	Error string `json:"erro"`
+}
+
 func DiseaseDetect(c *gin.Context) {
 
-	file, header, err := c.Request.FormFile("file")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	minioClient, err := bucket.CreateMinioClient("minio:9000", "MJ4Ile4FCe3tpjgqgKVx", "4aXSs4tygx9A85GfOvQ32W9ls8BDXZRqFeBQxmFb", "", false)
+
 	if err != nil {
+		myerror.HttpErrors(http.StatusBadRequest, "erro ao se conectar com o minio", c)
+		return
+	}
+
+	file, header, err := upload.UploadFile(c.Request, "image")
+	if err != nil {
+		myerror.HttpErrors(http.StatusBadRequest, err.Error(), c)
 		return
 	}
 
 	defer file.Close()
 
-	body := &bytes.Buffer{}
-	write := multipart.NewWriter(body)
-
-	part, err := write.CreateFormFile("image", header.Filename)
-	if err != nil {
+	if err := bucket.VerifyImageSize(header); err != nil {
+		myerror.HttpErrors(http.StatusRequestEntityTooLarge, err.Error(), c)
 		return
 	}
 
-	_, err = io.Copy(part, file)
-	if err != nil {
+	if err := bucket.VerifyImageType(header); err != nil {
+		myerror.HttpErrors(http.StatusUnsupportedMediaType, err.Error(), c)
 		return
 	}
 
-	write.Close()
+	bucketName := "images-analisys"
+	region := ""
+	objectLookin := false
 
-	url := "http://192.168.1.12:8000/detect-plant-disease"
-	method := "POST"
+	uploadIsSuccess, err := bucket.AsyncSendImageToBucket(ctx, minioClient, bucketName, file, *header, region, objectLookin)
 
-	req, err := http.NewRequest(method, url, body)
+	log.Println(uploadIsSuccess)
+
 	if err != nil {
+
+		if errors.Is(err, ctx.Err()) {
+			myerror.HttpErrors(http.StatusRequestTimeout, "tempo excedido", c)
+			return
+		}
+
+		myerror.HttpErrors(http.StatusInternalServerError, err.Error(), c)
 		return
 	}
-	req.Header.Set("Content-Type", write.FormDataContentType())
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	typeDetect := "disease"
+
+	urlImage := fmt.Sprintf("http://%s/%s/%s", "minio:9000", bucketName, header.Filename)
+
+	message, err := kafka.SendAndReceiver(ctx, urlImage, typeDetect)
+
 	if err != nil {
+
+		if errors.Is(err, ctx.Err()) {
+			myerror.HttpErrors(http.StatusRequestTimeout, err.Error(), c)
+			return
+		}
+
+		if errors.As(err, &sarama.ConsumerError{}) {
+			log.Println(err.Error())
+			myerror.HttpErrors(http.StatusInternalServerError, "erro ao consumir mensagem do kafka", c)
+			return
+		}
+
+		myerror.HttpErrors(http.StatusInternalServerError, "erro ao se comunicar com o servidor", c)
+	}
+
+	var responseApiPythonDisease ResponseApiPythonDisease
+
+	err = jsonutil.ConvertStringToJson(message, &responseApiPythonDisease)
+
+	if err != nil {
+		log.Println(err)
+		myerror.HttpErrors(http.StatusInternalServerError, err.Error(), c)
 		return
 	}
 
-	defer resp.Body.Close()
-
-	responseBody, _ := io.ReadAll(resp.Body)
-
-	c.Data(resp.StatusCode, resp.Header.Get("Content-Type"), responseBody)
-
+	c.JSON(http.StatusOK, responseApiPythonDisease)
 }
