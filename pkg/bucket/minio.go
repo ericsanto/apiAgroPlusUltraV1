222318808ac99be789b0005d9d1a4662c638666e
Eric Santos <ericjesus403@gmail.com>
2025-06-07 18:33:50 -0300

    feat: create function for create clinet minio

--- pkg/bucket/minio.go
+++ pkg/bucket/minio.go
@@ -5,10 +5,10 @@ import (
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
-func CreateMinioClient(endpoint string, accessKey, secretKe, id string, secure bool) (*minio.Client, error) {
+func CreateMinioClient(endpoint string, accessKey, secretKey, id string, secure bool) (*minio.Client, error) {
 
 	minioClient, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKey, secretKe, id),
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, id),
 		Secure: secure,
 	})
 
