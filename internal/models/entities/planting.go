ad0c205200a99a17ea847ec5f19b89db5ec63061
Eric Santos <ericjesus403@gmail.com>
2025-06-14 16:53:38 -0300

    feat: create relationship with Irrigation Type entity

--- internal/models/entities/planting.go
+++ internal/models/entities/planting.go
@@ -1,14 +1,19 @@
 package entities
 
-import "time"
+import (
+	"time"
+)
 
 type PlantingEntity struct {
 	ID                   uint `gorm:"primaryKey;autoIncrement"`
 	BatchID              uint
 	AgricultureCultureID uint
-	IsPlanting           bool                        `gorm:"default:false"`
-	StartDatePlanting    time.Time                   `gorm:"type:timestamp"`
-	ExpectedProduction   float64                     `gorm:"not null"`
+	IsPlanting           bool      `gorm:"default:false"`
+	StartDatePlanting    time.Time `gorm:"type:timestamp"`
+	ExpectedProduction   float64   `gorm:"not null"`
+	SpaceBetweenPlants   float64   `gorm:"not null"`
+	SpaceBetweenRows     float64   `gorm:"not null"`
+	IrrigationTypeID     uint
 	ProductionCost       []ProductionCostEntity      `gorm:"foreignKey:PlantingID"`
 	SalePlanting         []SalePlantingEntity        `gorm:"foreignKey:PlantingID"`
 	PerformancePlanting  []PerformancePlantingEntity `gorm:"foreignKey:PlantingID"`
