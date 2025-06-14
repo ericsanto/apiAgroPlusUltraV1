f76702b4c3800a4bd2bc3733367c38932a2eb0ea
Eric Santos <ericjesus403@gmail.com>
2025-06-14 17:00:24 -0300

    feat: add Irrigation Type field that returns the name of the irrigation type in the BatchPlantingResponse dto

--- internal/models/responses/batch_planties.go
+++ internal/models/responses/batch_planties.go
@@ -6,5 +6,9 @@ type BatchPlantiesResponse struct {
 	BatchName              string    `json:"batch_name"`
 	IsPlanting             bool      `json:"is_planting"`
 	AgricultureCultureName string    `json:"agriculture_culture_name"`
+	SoilTypeName           string    `json:"soil_type_name"`
 	StartDatePlanting      time.Time `json:"start_date_planting"`
+	SpaceBetweenPlants     float64   `json:"space_between_plants"`
+	SpaceBetweenRows       float64   `json:"space_between_rows"`
+	IrrigationType         string    `json:"irrigation_type"`
 }
