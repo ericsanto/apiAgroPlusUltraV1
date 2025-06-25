37267d3aa2e8ea987d468265707a0164e1dd520b
Eric Santos <ericjesus403@gmail.com>
2025-06-25 09:38:28 -0300

    feat: adds new fields to the PlantingRequest dto

--- internal/models/requests/planting.go
+++ internal/models/requests/planting.go
@@ -1,7 +1,13 @@
 package requests
 
+import "time"
+
 type PlantingRequest struct {
-	BatchID              uint  `json:"batch_id" validate:"required"`
-	AgricultureCultureID uint  `json:"agriculture_culture_id" validate:"required"`
-	IsPlanting           *bool `json:"is_planting" validate:"required"`
+	BatchID              uint      `json:"batch_id" validate:"required"`
+	AgricultureCultureID uint      `json:"agriculture_culture_id" validate:"required"`
+	IsPlanting           *bool     `json:"is_planting" validate:"required"`
+	StartDatePlanting    time.Time `json:"start_date_planting"`
+	SpaceBetweenPlants   float64   `json:"space_between_plants"`
+	SpaceBetweenRows     float64   `json:"space_between_rows"`
+	IrrigationTypeID     uint      `json:"irrigation_type_id"`
 }
