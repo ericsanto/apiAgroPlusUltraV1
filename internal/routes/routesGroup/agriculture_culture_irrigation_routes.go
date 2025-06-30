001faefedb72bdc494118a7083065dfa5f0faad2
Eric Santos <ericjesus403@gmail.com>
2025-06-30 18:58:27 -0300

    refactor(router): use DI builder to inject AgricultureCultureIrrigationController

--- internal/routes/routesGroup/agriculture_culture_irrigation_routes.go
+++ internal/routes/routesGroup/agriculture_culture_irrigation_routes.go
@@ -1,19 +1,15 @@
 package routesgroup
 
 import (
-	"github.com/ericsanto/apiAgroPlusUltraV1/config"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/controllers"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/middlewares"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/repositories"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/services"
 	"github.com/gin-gonic/gin"
+
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/di"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/middlewares"
 )
 
 func SetupRouterAgricultureCultureIrrigation(r *gin.Engine) {
 
-	agricultureCultureIrrigationRepository := repositories.NewAgricultureCultureIrrigationRepository(config.DB)
-	agricultureCultureIrrigationService := services.NewAgricultureCultureIrrigationService(agricultureCultureIrrigationRepository)
-	agricultureCultureIrrigationController := controllers.NewAgricultureCultureIrrigationController(agricultureCultureIrrigationService)
+	agricultureCultureIrrigationController := di.NewAgricultureCultureIrrigationBuilder().Builder()
 
 	agricultureCultureIrrigationRouter := r.Group("/v1/irrigacao-cultura")
 	agricultureCultureIrrigationRouter.GET("/", middlewares.MiddlewareValidateQueryParamAgricultureCultureIrrigation(), agricultureCultureIrrigationController.GetAgricultureCultureIrrigationFindByIDController)
