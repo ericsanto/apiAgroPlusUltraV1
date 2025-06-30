ee30a71a5aa7fb98fbb2a544f60e7e9a920a8613
Eric Santos <ericjesus403@gmail.com>
2025-06-30 19:10:27 -0300

    refactor(router): use DI builder to inject AgricultureCulturePestMethodController

--- internal/routes/routesGroup/agriculture_pest_method.go
+++ internal/routes/routesGroup/agriculture_pest_method.go
@@ -1,19 +1,15 @@
 package routesgroup
 
 import (
-	"github.com/ericsanto/apiAgroPlusUltraV1/config"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/controllers"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/middlewares"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/repositories"
-	"github.com/ericsanto/apiAgroPlusUltraV1/internal/services"
 	"github.com/gin-gonic/gin"
+
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/di"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/middlewares"
 )
 
 func SetupAgricultureCulturePestMethod(r *gin.Engine) {
 
-	agricultureCulturePestMethodRepository := repositories.NewAgricultureCulturePestMethodRepository(config.DB)
-	agricultureCulturePestMethodService := services.NewAgricultureCulturePestMethodService(agricultureCulturePestMethodRepository)
-	agricultureCulturePestMethodController := controllers.NewAgricultureCulturePestMethodController(agricultureCulturePestMethodService)
+	agricultureCulturePestMethodController := di.NewAgricultureCulturePestMethodBuilder().Builder()
 
 	routerAgricultureCulturePestMethod := r.Group("/v1/controle-de-praga-agricultura")
 	routerAgricultureCulturePestMethod.POST("/", agricultureCulturePestMethodController.PostAgricultureCulturePestMethod)
