f93fb757a6fe3b24b4e8e41e022df09fa37ea8cb
Eric Santos <ericjesus403@gmail.com>
2025-07-01 10:46:00 -0300

    refactor: create DiseaseDetectService service using solid principles

--- internal/services/disease_detect.go
+++ internal/services/disease_detect.go
@@ -1,64 +1,82 @@
 package services
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"log"
-// 	"time"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-// 	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/bucket"
-// 	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/jsonutil"
-// 	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/kafka"
-// 	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/upload"
-// )
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/bucket"
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/jsonutil"
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/kafka"
+	"github.com/ericsanto/apiAgroPlusUltraV1/pkg/upload"
+)
 
-// func DiseaseDetect(formFile upload.UploadFileInterface, formKey string) (map[string]string, error) {
+type DiseaseDetectServiceInterface interface {
+	ServiceDiseaseDetect(formFile upload.UploadFileInterface, formKey string) (map[string]string, error)
+}
 
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+type DiseaseDetectService struct {
+	bucketClient  bucket.BucketClientInterface
+	imageValidate bucket.ImageValidateInterface
+	kafkaClient   kafka.Messaging
+}
 
-// 	defer cancel()
+func NewDiseaseDetect(bucketClient bucket.BucketClientInterface, imageValidate bucket.ImageValidateInterface, kafkaClient kafka.Messaging) DiseaseDetectServiceInterface {
+	return &DiseaseDetectService{
+		bucketClient:  bucketClient,
+		imageValidate: imageValidate,
+		kafkaClient:   kafkaClient,
+	}
+}
 
-// 	minioClient, err := bucket.CreateMinioClient(ENDPOINT, ACCESS_KEY_MINIO, SECRET_KEY_MINIO, "", false)
+func (dds *DiseaseDetectService) ServiceDiseaseDetect(formFile upload.UploadFileInterface, formKey string) (map[string]string, error) {
 
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 		return nil, fmt.Errorf("erro ao se conectar com minio")
-// 	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
-// 	file, header, err := upload.UploadFile(formFile, formKey)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	defer cancel()
 
-// 	defer file.Close()
+	file, header, err := upload.UploadFile(formFile, formKey)
+	if err != nil {
+		return nil, err
+	}
 
-// 	if err := bucket.ValidateImageSizeAndType(header); err != nil {
-// 		return nil, err
-// 	}
+	defer file.Close()
 
-// 	if err := bucket.UploadImageToBucket(ctx, minioClient, bucketName, file, header, region, objectLookin); err != nil {
-// 		return nil, err
-// 	}
+	if err := dds.imageValidate.ValidateImageSizeAndType(header); err != nil {
+		return nil, err
+	}
 
-// 	urlImage := fmt.Sprintf("http://%s/%s/%s", ENDPOINT, bucketName, header.Filename)
+	configsBucket := bucket.BucketConfig{
+		Name:          os.Getenv("BUCKET_NAME"),
+		ObjectLocking: false,
+		Region:        "",
+	}
 
-// 	typeDetect := "disease"
+	if err := dds.bucketClient.PutObject(ctx, configsBucket, header, file); err != nil {
+		return nil, err
+	}
 
-// 	message, err := kafka.SendAndReceiverKafkaService(ctx, urlImage, typeDetect)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	endpoint := os.Getenv("ENDPOINT")
 
-// 	var responseApiPython map[string]string
+	urlImage := fmt.Sprintf("http://%s/%s/%s", endpoint, configsBucket.Name, header.Filename)
 
-// 	fmt.Println(message)
+	typeDetect := "disease"
 
-// 	err = jsonutil.ConvertStringToJson(message, &responseApiPython)
-// 	log.Println(err)
+	message, err := dds.kafkaClient.SendAndReceiverService(ctx, urlImage, typeDetect)
+	if err != nil {
+		return nil, err
+	}
 
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	var responseApiPython map[string]string
 
-// 	return responseApiPython, nil
-// }
+	err = jsonutil.ConvertStringToJson(message, &responseApiPython)
+	log.Println(err)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return responseApiPython, nil
+}
