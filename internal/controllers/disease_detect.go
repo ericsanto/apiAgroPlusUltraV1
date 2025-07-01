d8eec640fffdfbd670db0a23a725d44e706db541
Eric Santos <ericjesus403@gmail.com>
2025-07-01 10:46:27 -0300

    refactor: create DiseaseDetectController controller  using solid principles

--- internal/controllers/disease_detect.go
+++ internal/controllers/disease_detect.go
@@ -1,46 +1,60 @@
 package controllers
 
-// import (
-// 	"context"
-// 	"errors"
-// 	"log"
-// 	"net/http"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
 
-// 	"github.com/IBM/sarama"
-// 	"github.com/gin-gonic/gin"
+	"github.com/IBM/sarama"
+	"github.com/gin-gonic/gin"
 
-// 	"github.com/ericsanto/apiAgroPlusUltraV1/internal/services"
-// 	myerror "github.com/ericsanto/apiAgroPlusUltraV1/myError"
-// )
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/services"
+	myerror "github.com/ericsanto/apiAgroPlusUltraV1/myError"
+)
 
-// func DiseaseDetectController(c *gin.Context) {
+type DiseaseDetectControllerInterface interface {
+	DiseaseDetect(c *gin.Context)
+}
 
-// 	ctx := context.Background()
+type DiseaseDetectController struct {
+	diseaseDetectService services.DiseaseDetectServiceInterface
+}
 
-// 	formKey := "image"
+func NewDiseaseDetectController(diseaseDetectService services.DiseaseDetectServiceInterface) DiseaseDetectControllerInterface {
+	return &DiseaseDetectController{diseaseDetectService: diseaseDetectService}
+}
 
-// 	responseApiPython, err := services.DiseaseDetect(c.Request, formKey)
+func (ddc *DiseaseDetectController) DiseaseDetect(c *gin.Context) {
 
-// 	if err != nil {
-// 		switch {
-// 		case errors.Is(err, ctx.Err()):
-// 			myerror.HttpErrors(http.StatusRequestTimeout, err.Error(), c)
-// 			return
-// 		case errors.As(err, &sarama.ConsumerError{}):
-// 			log.Println(err.Error())
-// 			myerror.HttpErrors(http.StatusBadGateway, "erro ao consultar servidor externo", c)
-// 			return
-// 		case errors.Is(err, myerror.ErrImageSizeToLarge):
-// 			myerror.HttpErrors(413, err.Error(), c)
-// 			return
-// 		case errors.Is(err, myerror.ErrUnsupportedImageType):
-// 			myerror.HttpErrors(http.StatusUnsupportedMediaType, err.Error(), c)
-// 			return
-// 		default:
-// 			myerror.HttpErrors(http.StatusInternalServerError, err.Error(), c)
-// 			return
-// 		}
-// 	}
+	ctx := context.Background()
 
-// 	c.JSON(http.StatusOK, responseApiPython)
-// }
+	formKey := "image"
+
+	responseApiPython, err := ddc.diseaseDetectService.ServiceDiseaseDetect(c.Request, formKey)
+	fmt.Println(responseApiPython)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, ctx.Err()):
+			myerror.HttpErrors(http.StatusRequestTimeout, err.Error(), c)
+			return
+		case errors.As(err, &sarama.ConsumerError{}):
+			log.Println(err.Error())
+			myerror.HttpErrors(http.StatusBadGateway, "erro ao consultar servidor externo", c)
+			return
+		case errors.Is(err, myerror.ErrImageSizeToLarge):
+			myerror.HttpErrors(413, err.Error(), c)
+			return
+		case errors.Is(err, myerror.ErrUnsupportedImageType):
+			myerror.HttpErrors(http.StatusUnsupportedMediaType, err.Error(), c)
+			return
+		default:
+			myerror.HttpErrors(http.StatusInternalServerError, err.Error(), c)
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, responseApiPython)
+}
