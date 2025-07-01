435201c44accde9902bb5aec8a2196950bb13714
Eric Santos <ericjesus403@gmail.com>
2025-07-01 10:48:52 -0300

    refactor(router): use DI builder to inject DiseadeDetectController

--- internal/routes/routesGroup/disease-detect.go
+++ internal/routes/routesGroup/disease-detect.go
@@ -1,15 +1,17 @@
 package routesgroup
 
-// import (
-// 	"github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
 
-// 	"github.com/ericsanto/apiAgroPlusUltraV1/internal/controllers"
-// 	"github.com/ericsanto/apiAgroPlusUltraV1/internal/middlewares"
-// )
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/di"
+	"github.com/ericsanto/apiAgroPlusUltraV1/internal/middlewares"
+)
 
-// func RouterGroupDiseaseDetect(r *gin.Engine) {
+func RouterGroupDiseaseDetect(r *gin.Engine) {
 
-// 	routerDiseaseDetectGroup := r.Group("/v1/disease-detect")
+	diseaseDetectContorller, _ := di.NewDiseaseDetectBuilder().Builder()
 
-// 	routerDiseaseDetectGroup.POST("/", middlewares.ValidateJWT(), controllers.DiseaseDetectController)
-// }
+	routerDiseaseDetectGroup := r.Group("/v1/disease-detect")
+
+	routerDiseaseDetectGroup.POST("/", middlewares.ValidateJWT(), diseaseDetectContorller.DiseaseDetect)
+}
