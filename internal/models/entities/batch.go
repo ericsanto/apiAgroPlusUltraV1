9bf009f5060fdd8de80fc5994974503036f33a75
Eric Santos <ericjesus403@gmail.com>
2025-07-08 14:50:13 -0300

    feat: creates a unique index to avoid creating identical lot names on the same farm

--- internal/models/entities/batch.go
+++ internal/models/entities/batch.go
@@ -2,9 +2,9 @@ package entities
 
 type BatchEntity struct {
 	ID       uint             `gorm:"primaryKey;autoIncrement"`
-	Name     string           `gorm:"size:50;not null;unique"`
+	Name     string           `gorm:"size:50;not null;uniqueIndex:idx_farm_name"`
 	Area     float32          `gorm:"not null"`
 	Unit     string           `gorm:"size:10;not null"`
-	Planting []PlantingEntity `gorm:"foreignKey:BatchID"`
-	FarmID   uint
+	Planting []PlantingEntity `gorm:"foreignKey:BatchID;"`
+	FarmID   uint             `gorm:"uniqueIndex:idx_farm_name"`
 }
