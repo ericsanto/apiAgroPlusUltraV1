32fe6dc5d1771dc94366869e91b1af8a90c388d0
Eric Santos <ericjesus403@gmail.com>
2025-07-17 13:55:47 -0300

    feat: creates index user_farm_name to ensure that a user cannot create two farms with the same name

--- internal/models/entities/farm.go
+++ internal/models/entities/farm.go
@@ -1,8 +1,8 @@
 package entities
 
 type FarmEntity struct {
-	ID     uint   `gorm:"primaryKey;autoIncrement"`
-	Name   string `gorm:"size:150;not null"`
-	UserID uint
+	ID     uint          `gorm:"primaryKey;autoIncrement"`
+	Name   string        `gorm:"size:150;not null;uniqueIndex:idx_user_farm_name"`
+	UserID uint          `gorm:"uniqueIndex:idx_user_farm_name"`
 	Batch  []BatchEntity `gorm:"foreignKey:FarmID"`
 }
